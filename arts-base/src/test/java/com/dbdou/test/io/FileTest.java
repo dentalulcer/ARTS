150724ba2c8b58963e63c9f8c381485cb4b37673
dentalulcer <no19168@gmail.com>
2019-03-28 01:43:38 +0800

    io review

---
+++ arts-base/src/test/java/com/dbdou/test/io/FileTest.java
@@ -0,0 +1,98 @@
+package com.dbdou.test.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Created by dentalulcer on 2019/3/28.
+ */
+public class FileTest {
+
+    private static final String TEST_BASE_DIR = File.separator + "Users" + File.separator + "a0000"
+            + File.separator + "test" + File.separator + "java_test";
+
+    public static void main(String[] args) {
+
+//        test1();
+//        test2();
+//        test3();
+//        test4();
+//        test5();
+        test6();
+
+    }
+
+    private static void test6() {
+        List<String> list = new ArrayList<>();
+        File file = new File(TEST_BASE_DIR);
+        test61(file, list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    private static void test61(File file, List<String> list) {
+        list.add(file.getAbsolutePath());
+        if (file.isDirectory() && file.listFiles() != null && file.listFiles().length > 0) {
+            for (int i = 0; i < file.listFiles().length; i++) {
+                test61(file.listFiles()[i], list);
+            }
+        }
+    }
+
+    private static void test5() {
+        File dir = new File(TEST_BASE_DIR);
+        String[] list = dir.list();
+        File[] files = dir.listFiles();
+        if (list == null || files == null) {
+            System.out.println("文件夹不存在");
+            return ;
+        }
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+            System.out.println(files[i].getAbsolutePath());
+        }
+    }
+
+    private static void test4() {
+        File dir = new File(TEST_BASE_DIR + File.separator + "dir2");
+        if (dir.isDirectory()) {
+            System.out.println("is dir");
+        } else {
+            System.out.println("is not dir");
+        }
+        dir.mkdir();
+        if (dir.isDirectory()) {
+            System.out.println("is dir");
+        } else {
+            System.out.println("is not dir");
+        }
+    }
+
+    private static void test3() {
+        File file = new File(TEST_BASE_DIR + File.separator + "hello2.txt");
+        if (file.exists()) {
+            file.delete();
+        } else {
+            System.out.println("file not exists");
+        }
+    }
+
+    private static void test2() {
+        System.out.println(File.separator);
+        System.out.println(File.pathSeparator);
+    }
+
+    private static void test1() {
+        File file = new File(TEST_BASE_DIR + File.separator + "hello2.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
